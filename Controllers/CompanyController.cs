2c944442e447423ff732e1bc4c2daceb9227106c
swatikajikar <swatikajikar12@gmail.com>
2026-07-01 12:49:00 +0530

    added

---
+++ Controllers/CompanyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMVCAppWithJenkins.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
